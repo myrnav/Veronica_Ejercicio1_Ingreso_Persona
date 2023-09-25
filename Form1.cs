65c77e6515114b5d8edb512816c7ae481e70d3a2
myrnav <111702860+myrnav@users.noreply.github.com>
2023-09-25 18:49:05 -0300

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace Veronica_Ejercicio1_Ingreso_Persona
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
